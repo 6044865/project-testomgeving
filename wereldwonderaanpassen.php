9aa8fef7271407d6bf10602f33af1d0b6beb5f7e
Guusje <6044865@mborijnland.nl>
2025-09-16 14:53:05 +0200

    jkkn

--- wereldwonderaanpassen.php
+++ wereldwonderaanpassen.php
@@ -47,6 +47,26 @@ $toegestaan = $rechten[$rol] ?? [];
 <!DOCTYPE html>
 <html lang="nl">
 <head>
+     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wereldwonderen</title>
+    <script src="https://kit.fontawesome.com/0c7c27ff53.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/stylesheet.css">
+    <script src="../project-testomgeving/js/index.js" defer></script>
+    <!-- Leaflet CSS -->
+<link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+<!-- Leaflet JS -->
+<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+
+ 
+    <meta name="description"
+      content="Codex Mundi is een digitaal archief van de 21 wereldwonderen. Ontdek informatie, foto's, verhalen en geschiedenis van de klassieke, nieuwe en natuurlijke wereldwonderen.">
+<meta name="keywords"
+      content="wereldwonderen, 7 wereldwonderen, nieuwe wereldwonderen, klassieke wereldwonderen, geschiedenis, cultuur, Codex Mundi, digitaal archief, erfgoed">
+ 
+ 
+ 
+    <meta name="author" content="A.Alhaji, G.Verpaalen">
     <meta charset="UTF-8">
     <title>Wereldwonder Aanpassen</title>
     <style>
@@ -55,6 +75,11 @@ $toegestaan = $rechten[$rol] ?? [];
         button { cursor: pointer; }
     </style>
 </head>
+
+<?php
+require_once "classWereldwonder.php";
+require_once "./includes/header.php";
+?>
 <body>
 <h1>Wereldwonder Aanpassen</h1>
 
@@ -115,5 +140,10 @@ $toegestaan = $rechten[$rol] ?? [];
 
     <button type="submit">Opslaan</button>
 </form>
+
+
+<?php
+require_once "./includes/footer.php";
+?>
 </body>
 </html>
